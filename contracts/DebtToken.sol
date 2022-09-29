e218398b58d7322d3ba447ab36ccd1b71022655f
conlot-crypto <stepcek80@gmail.com>
2022-09-29 03:25:06 -0500

    add pair and debtToken

---
+++ contracts/DebtToken.sol
@@ -0,0 +1,223 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.1;
+
+import {WrapperTokenBase} from "./WrapperToken.sol";
+import "./interfaces/IBSLendingPair.sol";
+import "./interfaces/IDebtToken.sol";
+import "./token/ERC20Permit.sol";
+import "./token/IERC20Details.sol";
+
+////////////////////////////////////////////////////////////////////////////////////////////
+/// @title DebtToken
+/// @author @conlot-crypto
+////////////////////////////////////////////////////////////////////////////////////////////
+
+contract DebtToken is IDebtToken, WrapperTokenBase {
+    /// @dev debt token version
+    uint256 constant public VERSION = 0x1;
+
+    /// @dev for introspection
+    bool constant isDebtToken = true;
+
+    /// @dev debt token delegate borrow message digest
+    bytes32 internal constant _DEBT_BORROW_DELEGATE_SIGNATURE_TYPE_HASH =
+        keccak256(
+            "BorrowDelegate(bytes32 warning,address from,address to,uint amount,uint256 nonce)"
+        );
+
+    /// @dev user delegated borrow allowances
+    mapping(address => mapping(address => uint256)) private _borrowAllowances;
+
+    /// @notice mapping of user to approval nonce
+    mapping(address => uint256) public userBorrowAllowanceNonce;
+
+    /// @notice initialize
+    function initialize(
+        address __owner,
+        address _underlying,
+        string memory _tokenName,
+        string memory _tokenSymbol,
+        IRewardDistributorManager _manager
+    ) external virtual override initializer {
+        require(__owner != address(0), "invalid owner");
+        require(_underlying != address(0), "invalid underlying");
+
+        _owner = __owner;
+        uint8 underlyingDecimal = IERC20Details(_underlying).decimals();
+        initializeERC20(_tokenName, _tokenSymbol, underlyingDecimal);
+        initializeERC20Permit(_tokenName);
+        underlying = _underlying;
+        rewardManager = _manager;
+    }
+
+    function principal(address _account) external view override returns (uint256) {
+        return _balances[_account];
+    }
+
+    /// @dev calculates the debt balance of account
+    function balanceOf(address _account) public view override(IERC20, ERC20) returns (uint256) {
+        return IBSLendingPair(_owner).borrowBalancePrior(_account);
+    }
+
+    /// @dev mint debt tokens to the debtOwner address
+    /// @param _debtOwner the address to mint the debt tokens to
+    /// @param _to the address requesting the debt, when (_to != _debtOwner) debtOwner must have 
+    /// delegated some borrow allowance to the _to address
+    /// @param _amount the amount of debt tokens to mint
+    function mint(address _debtOwner, address _to, uint256 _amount) external override onlyLendingPair {
+        require(_debtOwner != address(0), "INVALID_DEBT_OWNER");
+        if(_debtOwner != _to) {
+            _decreaseBorrowAllowance(_debtOwner, _to, _amount);
+        }
+        _rewardHook(address(0), _debtOwner);
+        _mint(_debtOwner, _amount);
+    }
+
+    function _mint(address _account, uint256 _amount) internal virtual override {
+        require(_account != address(0), "ERC20: mint to the zero address");
+
+        _totalSupply += _amount;
+        _balances[_account] = balanceOf(_account) + _amount;
+        emit Transfer(address(0), _account, _amount);
+    }
+
+
+    function owner() external view override returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @notice burn is an only owner function that allows the owner to burn  tokens from an input account
+     * @param _from is the address where the tokens will be burnt
+     * @param _amount is the amount of token to be burnt
+     **/
+    function burn(address _from, uint256 _amount)
+        external
+        override(IBSWrapperTokenBase)
+        onlyLendingPair
+    {
+        _rewardHook(_from, address(0));
+        _balances[_from] = balanceOf(_from) - _amount;
+        if (_amount > _totalSupply) {
+            _totalSupply = 0;
+        } else {
+            _totalSupply -= _amount;
+        }
+    }
+
+    function borrowAllowance(address _from, address _to) external view returns(uint256) {
+        return _borrowAllowances[_from][_to];
+    }
+
+    function delegateBorrow(address _to, uint256 _amount) external {
+        _delegateBorrowInternal(msg.sender, _to, _amount);
+    }
+
+    function _delegateBorrowInternal(address _from, address _to, uint256 _amount) internal {
+        require(_to != address(0), "INVALID_TO");
+
+        _borrowAllowances[_from][_to] = _amount;
+        emit DelegateBorrow(_from, _to, _amount, block.timestamp);
+    }
+
+    function delegateBorrowWithSignedMessage(
+        address _from,
+        address _to,
+        uint256 _amount,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external {
+        require(_to != address(0), "INVALID_TO");
+
+         bytes32 digest =
+            keccak256(
+                abi.encodePacked(
+                    "\x19\x01",
+                    _domainSeparatorV4(),
+                    keccak256(
+                        abi.encode(
+                            _DEBT_BORROW_DELEGATE_SIGNATURE_TYPE_HASH,
+                            keccak256(
+                                "You are delegating borrow to user, read more here: https://warp.finance/delegate"
+                            ),
+                            _from,
+                            _to,
+                            _amount,
+                            userBorrowAllowanceNonce[_from]++
+                        )
+                    )
+                )
+            );
+        
+        address recoveredAddress = ecrecover(digest, v, r, s);
+        require(recoveredAddress == _from, "INVALID_SIGNATURE");
+
+        _delegateBorrowInternal(_from, _to, _amount);
+    }
+
+    function _decreaseBorrowAllowance(address _from, address _to, uint256 _amount) internal {
+        _borrowAllowances[_from][_to] =  _borrowAllowances[_from][_to] - _amount;
+    }
+    
+    /// @notice used to increase the debt of the system
+    /// @param _amount is the amount to increase
+    function increaseTotalDebt(uint256 _amount) external override onlyLendingPair {
+        _totalSupply = _totalSupply + _amount;
+    }
+
+    function transfer(
+        address, /*recipient*/
+        uint256 /*amount*/
+    ) public pure override(ERC20, IERC20) returns (bool) {
+        revert("TRANSFER_NOT_SUPPORTED");
+    }
+
+    function approve(
+        address, /*spender*/
+        uint256 /*amount*/
+    ) public virtual override(ERC20, IERC20) returns (bool) {
+        revert("APPROVAL_NOT_SUPPORTED");
+    }
+
+    function allowance(
+        address, /*owner*/
+        address /*spender*/
+    ) public view virtual override(ERC20, IERC20) returns (uint256) {
+        revert("ALLOWANCE_NOT_SUPPORTED");
+    }
+
+    function transferFrom(
+        address, /*sender*/
+        address, /*recipient*/
+        uint256 /*amount*/
+    ) public virtual override(ERC20, IERC20) returns (bool) {
+        revert("TRANSFER_NOT_SUPPORTED");
+    }
+
+    function increaseAllowance(
+        address, /*spender*/
+        uint256 /*addedValue*/
+    ) public virtual override returns (bool) {
+        revert("ALLOWANCE_NOT_SUPPORTED");
+    }
+
+    function decreaseAllowance(
+        address, /*spender*/
+        uint256 /*subtractedValue*/
+    ) public virtual override returns (bool) {
+        revert("ALLOWANCE_NOT_SUPPORTED");
+    }
+
+    function permit(
+        address /* owner */,
+        address /* spender */,
+        uint256 /* amount */,
+        uint256 /* deadline */,
+        uint8 /* v */,
+        bytes32 /* r */,
+        bytes32 /* s */
+    ) public virtual override {
+        revert("PERMIT_NOT_SUPPORTED");
+    }
+}
---
+++ contracts/LendingPairFactory.sol
@@ -0,0 +1,295 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.1;
+
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+import "./interfaces/IPriceOracleAggregator.sol";
+import "./interfaces/IInterestRateModel.sol";
+import "./interfaces/IBSLendingPair.sol";
+import "./interfaces/IBSWrapperToken.sol";
+import "./interfaces/IDebtToken.sol";
+import "./interfaces/IRewardDistributorManager.sol";
+import "./interest/JumpRateModelV2.sol";
+import "./token/IERC20Details.sol";
+import "./DataTypes.sol";
+
+contract LendingPairFactory is Pausable {
+    using Clones for address;
+
+    address public owner;
+
+    address public lendingPairImplementation;
+    address public collateralWrapperImplementation;
+    address public debtTokenImplementation;
+    address public borrowAssetWrapperImplementation;
+    address public rewardDistributionManager;
+    address internal newOwner;
+
+    address[] public allPairs;
+
+    mapping(address => bool) public validInterestRateModels;
+
+    event NewLendingPair(address pair, uint256 created);
+    event LogicContractUpdated(address pairLogic);
+    event NewInterestRateModel(address ir, uint256 timestamp);
+    event OwnershipAccepted(address prevOwner, address newOwner, uint256 timestamp);
+    event TransferControl(address _newTeam, uint256 timestamp);
+
+    /// @notice modifier to allow only the owner to call a function
+    modifier onlyOwner() {
+        require(msg.sender == owner, "ONLY_OWNER");
+        _;
+    }
+
+    constructor(
+        address _owner,
+        address _pairLogic,
+        address _collateralWrapperLogic,
+        address _debtTokenLogic,
+        address _borrowAssetWrapperLogic,
+        address _rewardDistributionManager
+    ) {
+        require(_owner != address(0), "inv_o");
+        require(_pairLogic != address(0), "inv_l");
+        require(_collateralWrapperLogic != address(0), "inv_c");
+        require(_debtTokenLogic != address(0), "inv_d");
+        require(_borrowAssetWrapperLogic != address(0), "inv_b");
+        require(_rewardDistributionManager != address(0), "inv_r");
+
+        owner = _owner;
+        lendingPairImplementation = _pairLogic;
+        collateralWrapperImplementation = _collateralWrapperLogic;
+        debtTokenImplementation = _debtTokenLogic;
+        borrowAssetWrapperImplementation = _borrowAssetWrapperLogic;
+        rewardDistributionManager = _rewardDistributionManager;
+    }
+
+    /// @notice accept transfer of control
+    function acceptOwnership() external {
+        require(msg.sender == newOwner, "invalid owner");
+
+        // emit event before state change to do not trigger null address
+        emit OwnershipAccepted(owner, newOwner, block.timestamp);
+
+        owner = newOwner;
+        newOwner = address(0);
+    }
+
+    /// @notice Transfer control from current owner address to another
+    /// @param _newOwner The new team
+    function transferOwnership(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "INVALID_NEW_OWNER");
+        newOwner = _newOwner;
+        emit TransferControl(_newOwner, block.timestamp);
+    }
+
+    /// @notice pause
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /// @notice unpause
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function updatePairImpl(address _newLogicContract) external onlyOwner {
+        require(_newLogicContract != address(0), "INV_C");
+        lendingPairImplementation = _newLogicContract;
+        emit LogicContractUpdated(_newLogicContract);
+    }
+
+    function updateCollateralWrapperImpl(address _newLogicContract) external onlyOwner {
+        require(_newLogicContract != address(0), "INV_C");
+        collateralWrapperImplementation = _newLogicContract;
+        emit LogicContractUpdated(_newLogicContract);
+    }
+
+    function updateDebtTokenImpl(address _newLogicContract) external onlyOwner {
+        require(_newLogicContract != address(0), "INV_C");
+        debtTokenImplementation = _newLogicContract;
+        emit LogicContractUpdated(_newLogicContract);
+    }
+
+    function updateBorrowAssetWrapperImpl(address _newLogicContract) external onlyOwner {
+        require(_newLogicContract != address(0), "INV_C");
+        borrowAssetWrapperImplementation = _newLogicContract;
+        emit LogicContractUpdated(_newLogicContract);
+    }
+
+    function updateRewardManager(address _newManager) external onlyOwner {
+        require(_newManager != address(0), "INV_C");
+        rewardDistributionManager = _newManager;
+        emit LogicContractUpdated(_newManager);
+    }
+
+    struct NewLendingVaultIRLocalVars {
+        uint256 baseRatePerYear;
+        uint256 multiplierPerYear;
+        uint256 jumpMultiplierPerYear;
+        uint256 optimal;
+        uint256 borrowRateMaxMantissa;
+        uint256 blocksPerYear;
+    }
+
+    /// @dev create interest rate model
+    function createIR(NewLendingVaultIRLocalVars calldata _interestRateVars, address _team)
+        external
+        onlyOwner
+        returns (address ir)
+    {
+        require(address(_team) != address(0), "inv_t");
+
+        ir = address(
+            new JumpRateModelV2(
+                _interestRateVars.baseRatePerYear,
+                _interestRateVars.multiplierPerYear,
+                _interestRateVars.jumpMultiplierPerYear,
+                _interestRateVars.optimal,
+                _team,
+                _interestRateVars.borrowRateMaxMantissa,
+                _interestRateVars.blocksPerYear
+            )
+        );
+
+        validInterestRateModels[ir] = true;
+
+        emit NewInterestRateModel(ir, block.timestamp);
+    }
+
+    /// @dev disable interest rate model
+    function disableIR(address ir) external onlyOwner {
+        require(validInterestRateModels[ir] == true, "IR_NOT_EXIST");
+        validInterestRateModels[ir] = false;
+    }
+
+    struct BorrowLocalVars {
+        IERC20 borrowAsset;
+        uint256 initialExchangeRateMantissa;
+        uint256 reserveFactorMantissa;
+        uint256 collateralFactor;
+        uint256 liquidationFee;
+        IInterestRateModel interestRateModel;
+    }
+
+    struct WrappedAssetLocalVars {
+        IBSWrapperToken wrappedBorrowAsset;
+        IBSWrapperToken wrappedCollateralAsset;
+        IDebtToken debtToken;
+    }
+
+    /// @dev create lending pair with clones
+    function createLendingPairWithProxy(
+        DataTypes.LendingPairVars memory _pairVars,
+        DataTypes.RiskConfiguration memory _riskConfig,
+        BorrowLocalVars calldata _borrowVars
+    ) external whenNotPaused returns (address newLendingPair) {
+        require(_pairVars.guardian != address(0), "INV_G");
+        require(address(_pairVars.collateralAsset) != address(0), "INV_C");
+        require(address(_borrowVars.borrowAsset) != address(0), "INV_B");
+        require(validInterestRateModels[address(_borrowVars.interestRateModel)] == true, "INV_I");
+
+        WrappedAssetLocalVars memory wrappedAssetLocalVars;
+
+        bytes32 salt = keccak256(abi.encode(_pairVars.name, _pairVars.symbol, allPairs.length));
+        newLendingPair = lendingPairImplementation.cloneDeterministic(salt);
+
+        // initialize wrapper borrow asset
+        wrappedAssetLocalVars.wrappedBorrowAsset = IBSWrapperToken(
+            initWrapperTokensWithProxy(
+                borrowAssetWrapperImplementation,
+                newLendingPair,
+                address(_borrowVars.borrowAsset),
+                _pairVars.name,
+                "BOR",
+                salt
+            )
+        );
+
+        // initialize wrapper collateral asset
+        wrappedAssetLocalVars.wrappedCollateralAsset = IBSWrapperToken(
+            initWrapperTokensWithProxy(
+                collateralWrapperImplementation,
+                newLendingPair,
+                address(_pairVars.collateralAsset),
+                _pairVars.name,
+                "COL",
+                salt
+            )
+        );
+
+        // initialize debt token
+        wrappedAssetLocalVars.debtToken = IDebtToken(
+            initWrapperTokensWithProxy(
+                debtTokenImplementation,
+                newLendingPair,
+                address(_borrowVars.borrowAsset),
+                _pairVars.name,
+                "DEBT",
+                salt
+            )
+        );
+
+        DataTypes.BorrowAssetConfig memory borrowConfig =
+            DataTypes.BorrowAssetConfig(
+                _borrowVars.initialExchangeRateMantissa,
+                _borrowVars.reserveFactorMantissa,
+                _borrowVars.collateralFactor,
+                wrappedAssetLocalVars.wrappedBorrowAsset,
+                _borrowVars.liquidationFee,
+                wrappedAssetLocalVars.debtToken
+            );
+
+        // initialize lending pair
+        IBSLendingPair(newLendingPair).initialize(
+            _pairVars,
+            borrowConfig,
+            wrappedAssetLocalVars.wrappedCollateralAsset,
+            _borrowVars.interestRateModel,
+            _riskConfig
+        );
+
+        allPairs.push(newLendingPair);
+        emit NewLendingPair(newLendingPair, block.timestamp);
+    }
+
+    function initWrapperTokensWithProxy(
+        address _implementation,
+        address _pair,
+        address _underlying,
+        string memory _lendingPairName,
+        string memory _tokenType,
+        bytes32 _salt
+    ) internal returns (address wrapper) {
+        wrapper = _implementation.cloneDeterministic(_salt);
+
+        initializeWrapperTokens(
+            _pair,
+            IBSWrapperToken(wrapper),
+            IERC20Details(_underlying),
+            _lendingPairName,
+            _tokenType
+        );
+    }
+
+    function initializeWrapperTokens(
+        address _pair,
+        IBSWrapperToken _wrapperToken,
+        IERC20Details _underlying,
+        string memory _lendingPairName,
+        string memory _tokenType
+    ) internal {
+        bytes memory name = abi.encodePacked(_lendingPairName);
+        name = abi.encodePacked(name, "-PAIR-", _tokenType);
+        bytes memory symbol = abi.encodePacked(_lendingPairName);
+        symbol = abi.encodePacked(name, _tokenType);
+        // initialize wrapperToken
+        IBSWrapperToken(_wrapperToken).initialize(
+            _pair,
+            address(_underlying),
+            string(name),
+            string(symbol),
+            IRewardDistributorManager(rewardDistributionManager)
+        );
+    }
+}
